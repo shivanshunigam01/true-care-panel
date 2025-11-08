8a198738073118a2f3a10a345c3781b57e76b0a9
shivanshunigam01 <shivanshunigam8@gmail.com>
2025-11-08 21:40:22 +0530

    asdasd

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import { Lead } from "@/types/leads";
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "https://api.truehospitals.com/leads";
+  import.meta.env.VITE_API_URL || "https://api.truehospitals.com/api/leads";
 
 export const getLeads = async (): Promise<Lead[]> => {
   const { data } = await axios.get(API_URL);
